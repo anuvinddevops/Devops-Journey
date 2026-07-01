6416ce020c5d8090794d3ca2abd41aa653924c00
Anuvind <anuvinddevops@gmail.com>
2026-07-01 07:06:24 +0000

    added system info bash script

---
+++ sysinfo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "=============================="
+echo "SYSTEM INFORMATION"
+echo "=============================="
+
+echo "Date and Time:"
+date
+
+echo"Logged in as:"
+whoami
+
+echo "Current directory:"
+pwd
+
+echo "Disk space:"
+df -h
+
+echo "Memory usage:"
+free -h
+
+echo "=============================="
+echo "End of System Report"
+echo "=============================="
+
